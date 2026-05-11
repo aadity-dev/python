bf8be20852c70b3cabfc41ab5fe4a98403677525
Adi-112 <adp6943@gmail.com>
2026-05-11 16:23:06 +0530

    split and join

---
+++ revpy/split_join.py
@@ -0,0 +1,16 @@
+def split_and_join(line):
+    # write your code here
+    words = line.split(" ")
+    result = ""
+    
+    for i in range(len(words)):
+        result += words[i]
+        
+        if i < len(words) - 1:
+            result += "-"
+            
+    return result
+if __name__ == '__main__':
+    line = input()
+    result = split_and_join(line)
+    print(result)
